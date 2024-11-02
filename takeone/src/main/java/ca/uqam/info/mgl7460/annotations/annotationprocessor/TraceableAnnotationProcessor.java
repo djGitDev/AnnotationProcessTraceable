281b1084c4f3b7cac6a1068ebf8f7c2a46a88df9
djawad <ha191952@ens.uqam.ca>
2024-11-01 20:07:30 -0400

    Generation des methodes

--- takeone/src/main/java/ca/uqam/info/mgl7460/annotations/annotationprocessor/TraceableAnnotationProcessor.java
+++ takeone/src/main/java/ca/uqam/info/mgl7460/annotations/annotationprocessor/TraceableAnnotationProcessor.java
@@ -6,6 +6,7 @@ import com.google.auto.service.AutoService;
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.*;
+import javax.lang.model.type.TypeMirror;
 import javax.tools.JavaFileObject;
 import java.io.IOException;
 import java.io.Writer;
@@ -65,8 +66,8 @@ public class TraceableAnnotationProcessor  extends AbstractProcessor   {
 
                         StringBuilder texteAGenerer = new StringBuilder();
                         ecrireEnteteClasse(classeDeLaMethodeAnnotee,texteAGenerer);
-                        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
-                        System.out.println(texteAGenerer);
+                        ecrireAttributLogger(classeDeLaMethodeAnnotee,texteAGenerer);
+
 
                         // Obtenir les membres de la classe
                         List<? extends Element> elementsMembres = classeDeLaMethodeAnnotee.getEnclosedElements();
@@ -77,16 +78,19 @@ public class TraceableAnnotationProcessor  extends AbstractProcessor   {
                         // Isoler les constructeurs membres dans une liste
                         List<Element> membresConstructeurs = filterElementsParGenre(elementsMembres,ElementKind.CONSTRUCTOR);
 
-
-                        // Afficher les membres
-                        for (Element Element : membresMethods) {
-                            System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
-                            System.out.println("Membre : " + Element.getSimpleName().toString());
-                        }
                         for (Element Element : membresConstructeurs) {
                             System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
                             System.out.println("Membre : " + Element.getSimpleName().toString());
                         }
+
+                        // Afficher les membres
+                        for (Element Element : membresMethods) {
+                            ecrireMethodeCourante((ExecutableElement)Element,texteAGenerer);
+                        }
+
+                            System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
+                            System.out.println(texteAGenerer);
+
                         //Afficher le nom de la classe encapsulante
                             System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
                         System.out.println(nomDeclassDeElementAnnote.toString());
@@ -98,6 +102,9 @@ public class TraceableAnnotationProcessor  extends AbstractProcessor   {
 
         return true;
     }
+
+
+
 
     private List<Element> filterElementsParGenre(List<? extends javax.lang.model.element.Element> elementsMembres, ElementKind genre){
         return elementsMembres.stream().filter(e -> e.getKind() == genre).collect(Collectors.toList());
@@ -113,10 +120,92 @@ public class TraceableAnnotationProcessor  extends AbstractProcessor   {
                 .append("import java.util.logging.ConsoleHandler"+ ";\n")
                 .append("public class "+ nomClasseGeneree + " extends " + nomClasseMere+ "{\n" );
     }
+    private void ecrireAttributLogger(TypeElement classeDeLaMethodeAnnotee, StringBuilder texteAGenerer) {
+        texteAGenerer.append("\tprivate static Logger logger = null;\n")
+                .append("\tstatic{\n\t\tlogger = logger.getLogger(\"" +classeDeLaMethodeAnnotee.getQualifiedName() + "\");\n" )
+                .append("\t\tlogger.addHandler(new ConsoleHandler());\n\t}\n");
+    }
+
+    private void ecrireMethodeCourante(ExecutableElement methode, StringBuilder texteAGenerer) {
+        // Ajouter les annotations avant les modificateurs et le nom de la méthode
+        if (verifierPresenceAnnotationTraceable(methode)) {
+            redefinirMethodeAnnotee(methode, texteAGenerer);
+        } else {
+            redefinirMethodeNonAnnotee(methode, texteAGenerer);
+        }
+    }
+
+    private void redefinirMethodeNonAnnotee(ExecutableElement methode, StringBuilder texteAGenerer) {
+        ecrireEnteteMethode(methode,texteAGenerer);
+        ecrireContenuMethodeNonAnnotee(methode,texteAGenerer);
+    }
+
+
+
+    private void redefinirMethodeAnnotee(ExecutableElement methode, StringBuilder texteAGenerer) {
+        ecrireEnteteAnnotation(texteAGenerer);
+        ecrireEnteteMethode(methode,texteAGenerer);
+        ecrireContenuMethodeAnnotee(methode,texteAGenerer);
+    }
+
+    private void ecrireContenuMethodeAnnotee(ExecutableElement methode, StringBuilder texteAGenerer) {
+        ecrireContenuAdditionnel(methode,texteAGenerer);
+        ecrireContenuMethodeNonAnnotee(methode,texteAGenerer);
+    }
+
+    private void ecrireContenuAdditionnel(ExecutableElement methode, StringBuilder texteAGenerer) {
+        Traceable annotation = methode.getAnnotation(Traceable.class);
+        String levelAnnote = annotation.level();
+        texteAGenerer.append("\t\tlogger.log(Level." + levelAnnote + ", \"Call to ")
+                .append(methode.getSimpleName().toString()).append("(");
+        List<? extends VariableElement> parameters = methode.getParameters();
+        for (VariableElement parameter : parameters) {
+            String param = parameter.getSimpleName().toString();
+            texteAGenerer.append(param +": \" + "+ param + " + \")\"").append(", ");
+        }
+        texteAGenerer.delete(texteAGenerer.length() - 2, texteAGenerer.length());
+        texteAGenerer.append(");\n");
+    }
+
+    private void ecrireContenuMethodeNonAnnotee(ExecutableElement methode, StringBuilder texteAGenerer) {
+        texteAGenerer.append("\t\tsuper.")
+                .append(methode.getSimpleName().toString())
+                .append("(");
+        List<? extends VariableElement> parameters = methode.getParameters();
+        for (VariableElement parameter : parameters) {
+            texteAGenerer.append(parameter.getSimpleName().toString()).append(", ");
+        }
+        texteAGenerer.delete(texteAGenerer.length() - 2, texteAGenerer.length());
+        texteAGenerer.append(");\n\t}");
+    }
+    private void ecrireEnteteMethode(ExecutableElement methode, StringBuilder texteAGenerer) {
+        Set<Modifier> modifiers = methode.getModifiers();
+        for (Modifier mod : modifiers) {
+            texteAGenerer.append("\t").append(mod.toString()).append(" ");
+        }
+        TypeMirror typeDeRetour = methode.getReturnType();
+        texteAGenerer.append(typeDeRetour.toString()).append(" ");
+        Name nomMethode = methode.getSimpleName();
+        texteAGenerer.append(nomMethode.toString());
+        List<? extends VariableElement> parameters = methode.getParameters();
+        texteAGenerer.append("(");
+        for (VariableElement param : parameters) {
+            texteAGenerer.append(param.asType().toString()).append(" ").append(param.getSimpleName().toString()).append(", ");
+        }
+        texteAGenerer.delete(texteAGenerer.length() - 2, texteAGenerer.length());
+        texteAGenerer.append(") {\n");
+    }
+
+    private void ecrireEnteteAnnotation(StringBuilder texteAGenerer) {
+        texteAGenerer.append("\t@Override\n");
+    }
 
     private String recupererNomPackage(TypeElement classeDeLaMethodeAnnotee) {
         PackageElement packageElement = (PackageElement) (classeDeLaMethodeAnnotee.getEnclosingElement());
             return packageElement.getQualifiedName().toString();
+    }
+    private boolean verifierPresenceAnnotationTraceable(ExecutableElement methode){
+        return methode.getAnnotation(Traceable.class) != null ;
     }
 
 
